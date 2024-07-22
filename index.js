987867808fb08ffee3d7c8bb42fc4db7e27b6158
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-07-22 08:41:25 -0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+// Faça um script de javascript que gerencia (CRUD) as atividades que o usuário fez nas férias, o usuário diz o dia e as atividades que realizou no dia.
+
+// Será utilizado:
+// Variáveis
+// Array
+// Objeto
+// Condicionais (utilize tando switch, como if else)
+// Laços de repetição (utilize forEach)
+// Entrada e Saída
+// Funções (Utilize function, arrow function e função anônima)
+// Módulos (Toda a lógica referente às funções de CRUD e o armazenamento deve estar em um módulo separado do arquivo principal)
+// Git/Github (Crie um repositório publico, e envie nesta atividade o link)
+// NPM (Utilize o pacote prompt-sync para interagir com usuário)
